098de1629f50206124c39f2bc48fdcb77bbacb60
Anurag Kumar <anuragxone@outlook.in>
2023-08-27 21:34:02 +0530

    add code for new connection

--- src/main/java/x/anurag/Main.java
+++ src/main/java/x/anurag/Main.java
@@ -1,7 +1,16 @@
 package x.anurag;
 
+import it.auties.whatsapp.api.QrHandler;
+import it.auties.whatsapp.api.Whatsapp;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Whatsapp.webBuilder()
+                .newConnection()
+                .unregistered(QrHandler.toTerminal())
+                .addLoggedInListener(() -> System.out.println("Connected!"))
+                .connect()
+                .join();
+
     }
 }
