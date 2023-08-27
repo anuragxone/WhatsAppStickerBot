05295fd1ef7821d205cc4b7b2b807b09576e104f
Anurag Kumar <anuragxone@outlook.in>
2023-08-27 21:14:12 +0530

    Initial project files

---
+++ src/main/java/x/anurag/Main.java
@@ -0,0 +1,7 @@
+package x.anurag;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/x/anurag/StickerBot.java
@@ -0,0 +1,37 @@
+package x.anurag;
+
+import it.auties.whatsapp.api.QrHandler;
+import it.auties.whatsapp.api.Whatsapp;
+import it.auties.whatsapp.model.info.MessageInfo;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class StickerBot {
+    public static void main(String[] args) {
+        Whatsapp.webBuilder()
+                .lastConnection()
+                .registered().get()
+                .addLoggedInListener(() -> System.out.println("Connected!"))
+                .addNewMessageListener(StickerBot::onMessage)
+                .connect()
+                .join();
+
+    }
+
+    private static void onMessage(Whatsapp api, MessageInfo info){
+        var imageMessage = info
+                .message()
+                .imageMessage()
+                .get();
+        Path path = FileSystems.getDefault().getPath(".", "downloadedImages", "image.jpg");
+
+        imageMessage.save(path);
+
+
+    }
+
+
+}
